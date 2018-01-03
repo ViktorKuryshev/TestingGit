1a758172bd72c26ecb10eb58abe23a11a6badcc4
Victor Kuryshev <sales@gtmk.ru>
2018-01-03 11:01:16 +0300

    Рабочий парсер для разбора

---
+++ TestingGit/Core/Habra/HabraSettings.cs
@@ -0,0 +1,19 @@
+﻿namespace TestingGit.Core.Habra
+{
+	class HabraSettings : IParserSettings
+	{
+		public HabraSettings(int start, int end)
+		{
+			StartPoint = start;
+			EndPoint = end;
+		}
+
+		public string BaseUrl { get; set; } = "https://habrahabr.ru";
+
+		public string Prefix { get; set; } = "page{CurrentId}";
+
+		public int StartPoint { get; set; }
+
+		public int EndPoint { get; set; }
+	}
+}
---
+++ TestingGit/ParseSiteForm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+using AngleSharp;
+using TestingGit.Core;
+using TestingGit.Core.Habra;
+
+namespace TestingGit
+{
+
+	public partial class ParseSiteForm : Form
+	{
+		ParserWorker<string[]> parser;
+		public ParseSiteForm()
+		{
+			InitializeComponent();
+			parser = new ParserWorker<string[]>(
+				new HabraParser()
+				
+				);
+			parser.OnCompleted += Parser_OnCompleted;
+			parser.OnNewData += Parser_OnNewData;
+		}
+
+		private void Parser_OnNewData(object arg1, string[] arg2)
+		{
+			listTitles.Items.AddRange(arg2);
+		}
+
+		private void Parser_OnCompleted(object obj)
+		{
+			MessageBox.Show("All done!");
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			parser.ParserSettings = new HabraSettings((int)numericStart.Value, (int)numericEnd.Value);
+			parser.Start();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			parser.Abort();
+		}
+	}
+}
